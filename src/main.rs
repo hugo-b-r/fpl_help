8cf2d670c38ba3a67d0d88ca3c4ff81b70e3b4ef
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-02 20:22:09 +0100

    move into a -side- function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::io::{prelude::*, BufReader};
 use dms_coordinates::DMS;
 
 fn main() {
-    let osm = Openstreetmap::new();
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -17,52 +16,7 @@ fn main() {
         process::exit(1);
     });
 
-    for address in file.addresses {
-        let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
-        
-        let longitude_decimal = res[0].x();
-        let latitude_decimal = res[0].y();
-        
-        let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
-        let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, false);
-
-
-        let latitude_deg_str: String;
-        let latitude_min_str: String;
-        let longitude_deg_str: String;
-        let longitude_min_str: String;
-
-        if longitude_dms.degrees < 10 {
-            longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
-        } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
-            longitude_deg_str = format!("0{}", longitude_dms.degrees);
-        } else {
-            longitude_deg_str = longitude_dms.degrees.to_string();
-        }
-        if longitude_dms.minutes < 10 {
-            longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
-        } else {
-            longitude_min_str = longitude_dms.minutes.to_string();
-        }
-        if latitude_dms.degrees < 10 {
-            latitude_deg_str = format!("0{}", latitude_dms.degrees);
-        } else {
-            latitude_deg_str = latitude_dms.degrees.to_string();
-        }
-        if latitude_dms.minutes < 10 {
-            latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
-        } else {
-            latitude_min_str = latitude_dms.minutes.to_string();
-        }
-
-        println!(
-            "{}{}N{}{}E",
-            latitude_deg_str,
-            latitude_min_str,
-            longitude_deg_str,
-            longitude_min_str
-        );
-    }
+    println!( "{}", get_addresses(file).unwrap() );
 }
 
 struct Config {
@@ -108,4 +62,58 @@ impl FPL {
             addresses,
         })
     }
+}
+
+fn get_addresses(file: FPL) -> Result<String, String> {
+
+    let osm = Openstreetmap::new();
+
+    let mut output: String = "".to_string();
+
+    for address in file.addresses {
+        let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
+        
+        let longitude_decimal = res[0].x();
+        let latitude_decimal = res[0].y();
+        
+        let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
+        let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, false);
+
+
+        let latitude_deg_str: String;
+        let latitude_min_str: String;
+        let longitude_deg_str: String;
+        let longitude_min_str: String;
+
+        if longitude_dms.degrees < 10 {
+            longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
+        } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
+            longitude_deg_str = format!("0{}", longitude_dms.degrees);
+        } else {
+            longitude_deg_str = longitude_dms.degrees.to_string();
+        }
+        if longitude_dms.minutes < 10 {
+            longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
+        } else {
+            longitude_min_str = longitude_dms.minutes.to_string();
+        }
+        if latitude_dms.degrees < 10 {
+            latitude_deg_str = format!("0{}", latitude_dms.degrees);
+        } else {
+            latitude_deg_str = latitude_dms.degrees.to_string();
+        }
+        if latitude_dms.minutes < 10 {
+            latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
+        } else {
+            latitude_min_str = latitude_dms.minutes.to_string();
+        }
+        output.push_str(format!(
+                "{}{}N{}{}E\n",
+                latitude_deg_str,
+                latitude_min_str,
+                longitude_deg_str,
+                longitude_min_str
+            ).as_str());
+    }
+    Ok( output )
 }
