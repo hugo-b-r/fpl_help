fae92211c7da54a2984e4975f9d4423c053bfc3e
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-02 18:13:23 +0100

    method new for struct COnfig to get from arguments

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,16 @@ struct Config {
     file_name: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 1 {
+            return Err("Please give a file name!");
+        }
+
+        let file_name = args[1].clone();
+
+        Ok(Config {
+            file_name
+        })
+    }
+}
