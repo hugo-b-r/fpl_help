fae9504d93909668f317cad628a3c72e6aef0441
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-03 19:47:26 +0100

    new egui app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, process};
 use fpl_help::{Config, FPL, get_addresses};
+use eframe::egui;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,4 +15,29 @@ fn main() {
     });
 
     println!( "{}", get_addresses(file).unwrap() );
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Add an address");
+       });
+   }
 }
