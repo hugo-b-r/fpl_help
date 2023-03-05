e45f83d69af583b4726f809bdf04dfd80aef4cfb
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-05 21:46:19 +0100

    ui

--- src/lib.rs
+++ src/lib.rs
@@ -48,72 +48,80 @@ impl FPL {
 }
 
 pub fn get_coordinates(address: String) -> Result<String, String> {
-    
-    let osm = Openstreetmap::new();
+    if address != "" {
+        let osm = Openstreetmap::new();
 
-    let mut output: String = "".to_string();
-
-    let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
+        let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
+            
+        let longitude_decimal = res[0].x();
+        let latitude_decimal = res[0].y();
         
-    let longitude_decimal = res[0].x();
-    let latitude_decimal = res[0].y();
-      
-    let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
-    let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, true);
+        let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
+        let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, true);
 
-    let latitude_deg_str: String;
-    let latitude_min_str: String;
-    let longitude_deg_str: String;
-    let longitude_min_str: String;
+        let latitude_deg_str: String;
+        let latitude_min_str: String;
+        let longitude_deg_str: String;
+        let longitude_min_str: String;
 
-    let mut longitude_direction: String = "E".to_string();
-    let mut latitude_direction: String = "N".to_string();
+        let mut longitude_direction: String = "E".to_string();
+        let mut latitude_direction: String = "N".to_string();
 
-    if longitude_dms.bearing.is_western() {
-        longitude_direction = "W".to_string();
-    }
-    if latitude_dms.bearing.is_southern() {
-        latitude_direction = "S".to_string();
-    }
-        
+        if longitude_dms.bearing.is_western() {
+            longitude_direction = "W".to_string();
+        }
+        if latitude_dms.bearing.is_southern() {
+            latitude_direction = "S".to_string();
+        }
+            
 
-    if longitude_dms.degrees < 10 {
-        longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
-    } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
-        longitude_deg_str = format!("0{}", longitude_dms.degrees);
+        if longitude_dms.degrees < 10 {
+            longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
+        } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
+            longitude_deg_str = format!("0{}", longitude_dms.degrees);
+        } else {
+            longitude_deg_str = longitude_dms.degrees.to_string();
+        }
+        if longitude_dms.minutes < 10 {
+            longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
+        } else {
+            longitude_min_str = longitude_dms.minutes.to_string();
+        }
+        if latitude_dms.degrees < 10 {
+            latitude_deg_str = format!("0{}", latitude_dms.degrees);
+        } else {
+            latitude_deg_str = latitude_dms.degrees.to_string();
+        }
+        if latitude_dms.minutes < 10 {
+            latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
+        } else {
+            latitude_min_str = latitude_dms.minutes.to_string();
+        }
+        let output: String = format!(
+            "{}{}{}{}{}{}\n",
+            latitude_deg_str,
+            latitude_min_str,
+            latitude_direction,
+            longitude_deg_str,
+            longitude_min_str,
+            longitude_direction
+        );
+        Ok( output )
     } else {
-        longitude_deg_str = longitude_dms.degrees.to_string();
+        Err("Not an address.".to_string())
     }
-    if longitude_dms.minutes < 10 {
-        longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
-    } else {
-        longitude_min_str = longitude_dms.minutes.to_string();
-    }
-    if latitude_dms.degrees < 10 {
-        latitude_deg_str = format!("0{}", latitude_dms.degrees);
-    } else {
-        latitude_deg_str = latitude_dms.degrees.to_string();
-    }
-    if latitude_dms.minutes < 10 {
-        latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
-    } else {
-        latitude_min_str = latitude_dms.minutes.to_string();
-    }
-    Ok(format!(
-        "{}{}{}{}{}{}\n",
-        latitude_deg_str,
-        latitude_min_str,
-        latitude_direction,
-        longitude_deg_str,
-        longitude_min_str,
-        longitude_direction
-    ))
 }
 
 pub fn get_coordinates_list(file: FPL) -> Result<String, String> {
     let mut output: String = "".to_string();
+    let mut coordinates: String;
     for address in file.addresses {
-        output.push_str(get_coordinates(address).unwrap().as_str());
+
+        coordinates = get_coordinates(address).unwrap_or_else(|err| {
+            eprintln!("Error when geocoding: {}", err);
+            "".to_string()
+        });
+        output.push_str(coordinates.as_str());
         thread::sleep(time::Duration::from_secs(1));
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, process};
-use fpl_help::{Config, FPL, get_addresses};
+use fpl_help::{Config, FPL, get_coordinates_list, get_coordinates};
 use eframe::egui;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,21 +11,25 @@ fn main() {
         process::exit(1);
     });
     let file = FPL::new(config).unwrap_or_else(|err| {
-        eprintln!("Problem found when loadign file: {}", err);
+        eprintln!("Problem found when loading file: {}", err);
         process::exit(1);
     });
 
-    println!( "{}", get_addresses(file).unwrap() );
+    println!( "{}", get_coordinates_list(file).unwrap() );
 
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+    eframe::run_native("FPL Help", native_options, Box::new(|cc| Box::new(FPLHelp::new(cc))));
 
 }
 
 #[derive(Default)]
-struct MyEguiApp {}
+struct FPLHelp {
+    address: String,
+    location_url: String,
+    coordinates: String,
+}
 
-impl MyEguiApp {
+impl FPLHelp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
@@ -34,10 +39,26 @@ impl MyEguiApp {
     }
 }
 
-impl eframe::App for MyEguiApp {
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Add an address");
+impl eframe::App for FPLHelp {
+    fn update(
+        &mut self,
+        ctx: &egui::Context,
+        frame: &mut eframe::Frame,
+    ) {
+        let Self { address, location_url, coordinates } = self;
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Add an address");
+            egui::menu::bar(ui, |ui| {
+                ui.text_edit_singleline(address);
+                if ui.button("Convert").clicked() {
+                    *coordinates = get_coordinates(address.clone()).unwrap_or_else(|err| {
+                        eprintln!("Error when geocoding: {}", err);
+                        "".to_string()
+                    });
+                };
+                ui.label(coordinates.as_str());
+           });
+           ui.hyperlink_to("Verify", location_url.as_str());
        });
    }
 }
