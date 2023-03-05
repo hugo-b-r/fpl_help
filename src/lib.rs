0230bef5c88a2cbe1b54f9005196ec0617578ec4
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-05 21:13:30 +0100

    separation of get_address in a get address to clarify

--- src/lib.rs
+++ src/lib.rs
@@ -47,70 +47,75 @@ impl FPL {
     }
 }
 
-pub fn get_addresses(file: FPL) -> Result<String, String> {
-
+fn get_address(address: String) -> Result<String, String> {
+    
     let osm = Openstreetmap::new();
 
     let mut output: String = "".to_string();
 
-    for address in file.addresses {
-        let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
+    let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
         
-        let longitude_decimal = res[0].x();
-        let latitude_decimal = res[0].y();
-        
-        let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
-        let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, true);
+    let longitude_decimal = res[0].x();
+    let latitude_decimal = res[0].y();
+      
+    let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
+    let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, true);
 
+    let latitude_deg_str: String;
+    let latitude_min_str: String;
+    let longitude_deg_str: String;
+    let longitude_min_str: String;
 
-        let latitude_deg_str: String;
-        let latitude_min_str: String;
-        let longitude_deg_str: String;
-        let longitude_min_str: String;
+    let mut longitude_direction: String = "E".to_string();
+    let mut latitude_direction: String = "N".to_string();
 
-        let mut longitude_direction: String = "E".to_string();
-        let mut latitude_direction: String = "N".to_string();
-
-        if longitude_dms.bearing.is_western() {
-            longitude_direction = "W".to_string();
-        }
-        if latitude_dms.bearing.is_southern() {
-            latitude_direction = "S".to_string();
-        }
-        
-
-        if longitude_dms.degrees < 10 {
-            longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
-        } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
-            longitude_deg_str = format!("0{}", longitude_dms.degrees);
-        } else {
-            longitude_deg_str = longitude_dms.degrees.to_string();
-        }
-        if longitude_dms.minutes < 10 {
-            longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
-        } else {
-            longitude_min_str = longitude_dms.minutes.to_string();
-        }
-        if latitude_dms.degrees < 10 {
-            latitude_deg_str = format!("0{}", latitude_dms.degrees);
-        } else {
-            latitude_deg_str = latitude_dms.degrees.to_string();
-        }
-        if latitude_dms.minutes < 10 {
-            latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
-        } else {
-            latitude_min_str = latitude_dms.minutes.to_string();
-        }
-        output.push_str(format!(
-                "{}{}{}{}{}{}\n",
-                latitude_deg_str,
-                latitude_min_str,
-                latitude_direction,
-                longitude_deg_str,
-                longitude_min_str,
-                longitude_direction
-            ).as_str());
-            thread::sleep(time::Duration::from_secs(1));
+    if longitude_dms.bearing.is_western() {
+        longitude_direction = "W".to_string();
     }
+    if latitude_dms.bearing.is_southern() {
+        latitude_direction = "S".to_string();
+    }
+        
+
+    if longitude_dms.degrees < 10 {
+        longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
+    } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
+        longitude_deg_str = format!("0{}", longitude_dms.degrees);
+    } else {
+        longitude_deg_str = longitude_dms.degrees.to_string();
+    }
+    if longitude_dms.minutes < 10 {
+        longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
+    } else {
+        longitude_min_str = longitude_dms.minutes.to_string();
+    }
+    if latitude_dms.degrees < 10 {
+        latitude_deg_str = format!("0{}", latitude_dms.degrees);
+    } else {
+        latitude_deg_str = latitude_dms.degrees.to_string();
+    }
+    if latitude_dms.minutes < 10 {
+        latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
+    } else {
+        latitude_min_str = latitude_dms.minutes.to_string();
+    }
+    Ok(format!(
+        "{}{}{}{}{}{}\n",
+        latitude_deg_str,
+        latitude_min_str,
+        latitude_direction,
+        longitude_deg_str,
+        longitude_min_str,
+        longitude_direction
+    ))
+}
+
+pub fn get_addresses(file: FPL) -> Result<String, String> {
+    let mut output: String = "".to_string();
+    for address in file.addresses {
+        output.push_str(get_address(address).unwrap().as_str());
+        thread::sleep(time::Duration::from_secs(1));
+    }
+
     Ok( output )
 }
