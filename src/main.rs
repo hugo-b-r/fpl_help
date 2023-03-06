c62c51d49f7f5b00461afce89e0bb6f53c56ef68
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-06 20:56:43 +0100

    fix warnings

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,13 @@ fn main() {
         min_window_size: Some(egui::vec2(480.0, 300.0)),
         ..Default::default()
     };
-    eframe::run_native("FPL Help", native_options, Box::new(|cc| Box::new(FPLHelp::new(cc))));
+    let _ = eframe::run_native("FPL Help", native_options, Box::new(|cc| Box::new(FPLHelp::new(cc))));
 
 }
 
 struct FPLHelp {
     clipboard: Clipboard,
     address: String,
-    location_url: String,
     coordinates: Vec<Point<f64>>,
 }
 
@@ -40,14 +39,13 @@ impl Default for FPLHelp {
         FPLHelp {
             clipboard: Clipboard::new().unwrap(),
             address: String::default(),
-            location_url: String::default(),
             coordinates: Vec::new(),
         }
     }
 }
 
 impl FPLHelp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
@@ -60,12 +58,11 @@ impl eframe::App for FPLHelp {
     fn update(
         &mut self,
         ctx: &egui::Context,
-        frame: &mut eframe::Frame,
+        _frame: &mut eframe::Frame,
     ) {
         let Self {
             clipboard,
             address,
-            location_url,
             coordinates 
         } = self;
         egui::CentralPanel::default().show(ctx, |ui| {
