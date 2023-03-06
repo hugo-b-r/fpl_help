19c306e4f18c2971d3678250db4d43678c02a5e3
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-06 18:42:24 +0100

    separates in differente function the part that is getting the coordinates and converts it

--- src/lib.rs
+++ src/lib.rs
@@ -47,69 +47,75 @@ impl FPL {
     }
 }
 
-pub fn get_coordinates(address: String) -> Result<String, String> {
+pub fn get_coordinates(address: String) -> Result<Vec<Point<f64>>, String> {
     if address != "" {
         let osm = Openstreetmap::new();
 
-        let res: Vec<Point<f64>> = osm.forward(&address).unwrap();
+        let output: Vec<Point<f64>> = osm.forward(&address).unwrap();
             
-        let longitude_decimal = res[0].x();
-        let latitude_decimal = res[0].y();
         
-        let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
-        let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, true);
-
-        let latitude_deg_str: String;
-        let latitude_min_str: String;
-        let longitude_deg_str: String;
-        let longitude_min_str: String;
-
-        let mut longitude_direction: String = "E".to_string();
-        let mut latitude_direction: String = "N".to_string();
-
-        if longitude_dms.bearing.is_western() {
-            longitude_direction = "W".to_string();
-        }
-        if latitude_dms.bearing.is_southern() {
-            latitude_direction = "S".to_string();
-        }
-            
-
-        if longitude_dms.degrees < 10 {
-            longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
-        } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
-            longitude_deg_str = format!("0{}", longitude_dms.degrees);
-        } else {
-            longitude_deg_str = longitude_dms.degrees.to_string();
-        }
-        if longitude_dms.minutes < 10 {
-            longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
-        } else {
-            longitude_min_str = longitude_dms.minutes.to_string();
-        }
-        if latitude_dms.degrees < 10 {
-            latitude_deg_str = format!("0{}", latitude_dms.degrees);
-        } else {
-            latitude_deg_str = latitude_dms.degrees.to_string();
-        }
-        if latitude_dms.minutes < 10 {
-            latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
-        } else {
-            latitude_min_str = latitude_dms.minutes.to_string();
-        }
-        let output: String = format!(
-            "{}{}{}{}{}{}\n",
-            latitude_deg_str,
-            latitude_min_str,
-            latitude_direction,
-            longitude_deg_str,
-            longitude_min_str,
-            longitude_direction
-        );
         Ok( output )
     } else {
         Err("Not an address.".to_string())
     }
+}
+
+fn convert_coordinates(coordinates: Point<f64>) -> Result<String, String> {
+    let longitude_decimal = coordinates.x();
+    let latitude_decimal = coordinates.y();
+    
+    let longitude_dms = DMS::from_decimal_degrees(longitude_decimal, false);
+    let latitude_dms = DMS::from_decimal_degrees(latitude_decimal, true);
+
+    let latitude_deg_str: String;
+    let latitude_min_str: String;
+    let longitude_deg_str: String;
+    let longitude_min_str: String;
+
+    let mut longitude_direction: String = "E".to_string();
+    let mut latitude_direction: String = "N".to_string();
+
+    if longitude_dms.bearing.is_western() {
+        longitude_direction = "W".to_string();
+    }
+    if latitude_dms.bearing.is_southern() {
+        latitude_direction = "S".to_string();
+    }
+        
+
+    if longitude_dms.degrees < 10 {
+        longitude_deg_str = format!("00{}", longitude_dms.degrees.to_string());
+    } else if longitude_dms.degrees < 100 { //longitude is max 180 compared to 90 for latitude
+        longitude_deg_str = format!("0{}", longitude_dms.degrees);
+    } else {
+        longitude_deg_str = longitude_dms.degrees.to_string();
+    }
+    if longitude_dms.minutes < 10 {
+        longitude_min_str = format!("0{}", longitude_dms.minutes.to_string());
+    } else {
+        longitude_min_str = longitude_dms.minutes.to_string();
+    }
+    if latitude_dms.degrees < 10 {
+        latitude_deg_str = format!("0{}", latitude_dms.degrees);
+    } else {
+        latitude_deg_str = latitude_dms.degrees.to_string();
+    }
+    if latitude_dms.minutes < 10 {
+        latitude_min_str = format!("0{}", latitude_dms.minutes.to_string());
+    } else {
+        latitude_min_str = latitude_dms.minutes.to_string();
+    }
+    let output: String = format!(
+        "{}{}{}{}{}{}\n",
+        latitude_deg_str,
+        latitude_min_str,
+        latitude_direction,
+        longitude_deg_str,
+        longitude_min_str,
+        longitude_direction
+    );
+
+    Ok( output)
 }
 
 pub fn get_coordinates_list(file: FPL) -> Result<String, String> {
