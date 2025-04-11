7601199b04279f3c651a8b91b52f1fb2884a60af
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2025-04-11 16:48:30 +0200

    Fix wrong symbol name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use fpl_help::{get_coordinates, convert_coordinates, url_from};
-use eframe::egui;
-use geocoding::Point;
 use arboard::Clipboard;
+use eframe::egui;
+use fpl_help::{convert_coordinates, get_coordinates, url_from};
+use geocoding::Point;
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
@@ -14,9 +14,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "FPL Help",
         native_options,
-        Box::new(
-            |cc| Box::new(FPLHelp::new(cc))
-        )
+        Box::new(|cc| Box::new(FPLHelp::new(cc))),
     )
 }
 
@@ -25,8 +23,8 @@ struct FPLHelp {
     address: String,
     coordinates: Vec<Point<f64>>,
     error: String,
-    flight_plan_coordinates: Vec<String>, 
-} //on stocke les différentes destinations sous la forme: 
+    flight_plan_coordinates: Vec<String>,
+} //on stocke les différentes destinations sous la forme:
 
 impl Default for FPLHelp {
     fn default() -> FPLHelp {
@@ -51,34 +49,33 @@ impl FPLHelp {
 }
 
 impl eframe::App for FPLHelp {
-    fn update(
-        &mut self,
-        ctx: &egui::Context,
-        _frame: &mut eframe::Frame,
-    ) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
             clipboard,
             address,
             coordinates,
             error,
-            flight_plan_coordinates
+            flight_plan_coordinates,
         } = self;
 
         let mut text: String = Default::default();
         let mut index = 0;
         for address in flight_plan_coordinates.clone().iter_mut() {
             if index >= 3 {
-                text.push_str(format!("{} ", &address[0..11]).as_str());    
+                text.push_str(format!("{} ", &address[0..11]).as_str());
                 index = 0;
             } else {
                 text.push_str(format!("{} ", &address[0..11]).as_str());
-            }   
+            }
             index += 1;
-        }    
+        }
         if text != *"" {
-            egui::TopBottomPanel::bottom("trip_coordinates").show(ctx, |ui| {            
-                ui.add_sized([egui::Ui::available_width(ui), 20.0], egui::TextEdit::multiline(&mut text));
-                
+            egui::TopBottomPanel::bottom("trip_coordinates").show(ctx, |ui| {
+                ui.add_sized(
+                    [egui::Ui::available_width(ui), 20.0],
+                    egui::TextEdit::multiline(&mut text),
+                );
+
                 ui.with_layout(egui::Layout::top_down(egui::Align::RIGHT), |ui| {
                     if ui.button("Copy complete trip").clicked() {
                         let _ = &clipboard.set_text(text);
@@ -107,50 +104,58 @@ impl eframe::App for FPLHelp {
                 ui.horizontal(|ui| {
                     ui.label(convert_coordinates(*point).unwrap());
                     ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
-                        #[cfg(not(traget_arch = "wasm32"))] //not copying or pasting on web
+                        #[cfg(not(target_arch = "wasm32"))] //not copying or pasting on web
                         if ui.button("Copy").clicked() {
                             let _ = &clipboard.set_text(convert_coordinates(*point).unwrap());
                             println!("copy {} to clipboard", convert_coordinates(*point).unwrap());
                         }
                         if ui.button("Add").clicked() {
-                            flight_plan_coordinates.push(format!("{} {}", convert_coordinates(*point).unwrap(), address));
-                            println!("Add {} to list of destinations", format!("{} {}", convert_coordinates(*point).unwrap(), address));
+                            flight_plan_coordinates.push(format!(
+                                "{} {}",
+                                convert_coordinates(*point).unwrap(),
+                                address
+                            ));
+                            println!(
+                                "Add {} to list of destinations",
+                                format!("{} {}", convert_coordinates(*point).unwrap(), address)
+                            );
                         }
                         ui.hyperlink_to("Verify coordinates", url_from(*point));
                     });
                 });
             }
 
-            if !flight_plan_coordinates.clone().is_empty() { // if no coordinates, we don't show
+            if !flight_plan_coordinates.clone().is_empty() {
+                // if no coordinates, we don't show
                 ui.add_space(12.0);
                 ui.separator();
                 ui.heading("Your planned flight");
             }
 
             egui::ScrollArea::vertical().show(ui, |ui| {
-
                 for coordinates in flight_plan_coordinates.clone().iter_mut() {
                     ui.horizontal(|ui| {
                         ui.label((*coordinates).clone());
                         ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
                             if ui.button("Remove").clicked() {
-                                let index = flight_plan_coordinates.clone().iter().position(|x| *x == *coordinates).unwrap();
+                                let index = flight_plan_coordinates
+                                    .clone()
+                                    .iter()
+                                    .position(|x| *x == *coordinates)
+                                    .unwrap();
                                 flight_plan_coordinates.remove(index);
                             }
                             if ui.button("Copy coordinates").clicked() {
-                                let _ = &clipboard.set_text((*coordinates)[0..11].to_string().clone());
+                                let _ =
+                                    &clipboard.set_text((*coordinates)[0..11].to_string().clone());
                             }
                             if ui.button("Copy all").clicked() {
                                 let _ = &clipboard.set_text((*coordinates).to_string().clone());
                             }
                         });
-                    });   
+                    });
                 }
-
-                
             });
-
-            
         });
     }
 }
